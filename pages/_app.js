6a93e1a5c854b02f56df35563df0b4c4892f89d9
Viet Nguyen <vtn1989@gmail.com>
2020-06-04 12:07:25 -0400

    update packages, update next to latest (9.4) (#3)

--- pages/_app.js
+++ pages/_app.js
@@ -3,25 +3,17 @@ import { ThemeProvider } from 'styled-components'
 
 import theme from '../styles/theme'
 
-export default class MyApp extends App {
-    static async getInitialProps({ Component, router, ctx }) {
-        let pageProps = {}
-
-        if (Component.getInitialProps) {
-            pageProps = await Component.getInitialProps(ctx)
-        }
-
-        return { pageProps }
-    }
-
-    render() {
-        const { Component, pageProps } = this.props
-        return (
-            <ThemeProvider theme={theme}>
-                <Component
-                    {...pageProps}
-                />
-            </ThemeProvider>
-        )
-    }
+function MyApp({ Component, pageProps }) {
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    )
 }
+
+MyApp.getInitialProps = async (appContext) => {
+    const appProps = await App.getInitialProps(appContext)
+    return { ...appProps }
+}
+
+export default MyApp
